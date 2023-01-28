e21716a0ebb0566fad2f225ab51338aef5fe2ea3
Ivan <koovalin@gmail.com>
2023-01-28 17:32:13 +0300

    Add HW_1_3

---
+++ Home Work 1/HW_1_3.py
@@ -0,0 +1,21 @@
+num = int(input("Число для проверки:\n"))
+
+def sum_all(n):
+    if n == 0:
+        return 0
+    else:
+        return n % 10 + sum_all(n // 10)
+
+def is_lucky(num):
+    if len(str(num)) != 6:
+        return print("Invalid")
+    a = sum_all(int(str(num)[3::]))
+    b = sum_all(int(str(num)[:3:]))
+    # print(a)
+    # print(b)
+    if a == b:
+        return print("Число счастливое!")
+    return print("Число не счастливое!")
+
+
+is_lucky(num)
