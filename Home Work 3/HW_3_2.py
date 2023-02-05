c5086aa131a71fc3edfbe6ac243b13c32976b3f7
Ivan <koovalin@gmail.com>
2023-02-05 03:29:50 +0300

    Modified HW_3_1

---
+++ Home Work 3/HW_3_2.py
@@ -0,0 +1,19 @@
+# word = "ноутбук"
+word = str(input("Введите слово для подсчета его стоимости: "))
+
+points = {'1' : "AEIOULNSTRАВЕИНОРСТ",
+          '2' : "DGДКЛМПУ",
+          '3' : "BCMPБГЁЬЯ",
+          '4' : "FHVWYЙЫ",
+          '5' : "KЖЗХЦЧ",
+          '8' : "JXШЭЮ",
+          '10': "QZФЩЪ"}
+
+cost = 0
+for letter in word.upper():
+    for key in points:
+        if letter in points[key]:
+            cost += int(key)
+print(cost)
+
+
